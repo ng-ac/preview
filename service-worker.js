a59cbdb50a8783eb0bd2b6de355cb1117c6ddffd
ng-ac <nagamoto217@gmail.com>
2025-10-29 11:17:13 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,48 @@
-const CACHE_NAME = 'nav-app-v1';
+const CACHE_NAME = 'nav-app-v2';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/mc.html',
-    '/htc.html',
-    '/masakari.html',
-    '/鉞組ロゴグレイ.PNG',
-    '/鉞組_丸ロゴ.png'
+  './main.html',
+  './mc.html',
+  './htc.html',
+  './masakari.html',
+  './鉞組ロゴグレイ.PNG',
+  './鉞組_丸ロゴ.png'
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(urlsToCache))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache).catch((err) => {
+          console.log('Cache error:', err);
+        });
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        return caches.match('./main.html');
+      })
+  );
 });
