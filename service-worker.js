d9111db721ed3bfc86f02670c6729118448776e7
ng-ac <nagamoto217@gmail.com>
2025-11-07 14:03:16 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
 const CACHE_NAME = 'yomogi-v1';
 const urlsToCache = [
     '/preview/yomogi/yomogi.html',
-    '/preview/yomogiphoto/よもぎ1.png',
-    '/preview/yomogiphoto/よもぎ2.png',
-    '/preview/yomogiphoto/よもぎ3.png',
-    '/preview/yomogiphoto/よもぎ4.png',
-    '/preview/yomogiphoto/よもぎ5.png',
-    '/preview/yomogiphoto/よもぎ6.png',
-    '/preview/yomogiphoto/よもぎ7.png',
-    '/preview/yomogiphoto/よもぎ8.png',
-    '/preview/yomogiphoto/よもぎ9.png'
+    '/preview/yomogiphoto/よもぎ1.jpg',
+    '/preview/yomogiphoto/よもぎ2.jpg',
+    '/preview/yomogiphoto/よもぎ3.jpg',
+    '/preview/yomogiphoto/よもぎ4.jpg',
+    '/preview/yomogiphoto/よもぎ5.jpg',
+    '/preview/yomogiphoto/よもぎ6.jpg',
+    '/preview/yomogiphoto/よもぎ7.jpg',
+    '/preview/yomogiphoto/よもぎ8.jpg',
+    '/preview/yomogiphoto/よもぎ9.jpg'
 ];
 
 self.addEventListener('install', (event) => {
@@ -25,4 +25,5 @@ self.addEventListener('fetch', (event) => {
             .then((response) => response || fetch(event.request))
     );
 });
+
 
