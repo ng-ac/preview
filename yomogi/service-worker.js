d2f97ecb4db35b46cc359c7c6d51f1cff79b7c2b
ng-ac <nagamoto217@gmail.com>
2025-11-07 09:52:50 +0900

    Update service-worker.js

--- yomogi/service-worker.js
+++ yomogi/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yomogi-v4';
+const CACHE_NAME = 'yomogi-v1';
 const urlsToCache = [
     '/preview/yomogi/yomogi.html',
     '/preview/yomogiphoto/よもぎ1.png',
@@ -12,39 +12,17 @@ const urlsToCache = [
     '/preview/yomogiphoto/よもぎ9.png'
 ];
 
-// インストール
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(CACHE_FILES))
-            .then(() => self.skipWaiting())
+            .then((cache) => cache.addAll(urlsToCache))
     );
 });
 
-// アクティベート
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys()
-            .then((cacheNames) => {
-                return Promise.all(
-                    cacheNames.map((cacheName) => {
-                        if (cacheName !== CACHE_NAME) {
-                            return caches.delete(cacheName);
-                        }
-                    })
-                );
-            })
-            .then(() => self.clients.claim())
-    );
-});
-
-// フェッチ
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
-            })
+            .then((response) => response || fetch(event.request))
     );
 });
 
