c84a30212f072fc3996493cc19aa5ce4febc2c49
ng-ac <nagamoto217@gmail.com>
2025-11-07 16:58:45 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,7 @@
 const CACHE_NAME = 'yomogi-v1';
 const urlsToCache = [
+    '/preview/main/main.html',
+    '/preview/main/main.jpg',
     '/preview/yomogi/yomogi.html',
     '/preview/yomogiphoto/よもぎ1.jpg',
     '/preview/yomogiphoto/よもぎ2.jpg',
@@ -12,18 +14,32 @@ const urlsToCache = [
     '/preview/yomogiphoto/よもぎ9.jpg'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(urlsToCache))
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
     );
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then((response) => response || fetch(event.request))
+        caches.match(event.request, { ignoreSearch: true }).then(response => {
+            return response || fetch(event.request).catch(() => {
+                // オフラインで未キャッシュのリソースの場合、main.htmlを代わりに返す
+                if (event.request.mode === 'navigate') {
+                    return caches.match('/preview/main/main.html');
+                }
+            });
+        })
     );
 });
-
 
